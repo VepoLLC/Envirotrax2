f00e9ef0434d6ec213d7a8e74108de946f46f82d
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-12-06 20:21:12 -0800

    Added Envirotrax.Auth project

---
+++ src/Envirotrax.Auth/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Envirotrax.Auth.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+

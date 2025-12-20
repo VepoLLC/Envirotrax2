5a9d0bdd71ca55f3d1c58ad7c1ce70af21eb47fb
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-12-20 11:46:45 -0800

    Pages for managing water suppliers

--- src/Envirotrax.App/envirotrax.app.client/src/app/admin/water-suppliers/water-supplier.module.ts
+++ src/Envirotrax.App/envirotrax.app.client/src/app/admin/water-suppliers/water-supplier.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { SharedComponentsModule } from "../../shared/components/shared.components.module";
 import { WaterSupplierRoutingModule } from "./water-supplier-routing.module";
+import { CreateSupplierComponent } from "./create/create-supplier.component";
+import { EditSupplierComponent } from "./edit/edit-supplier.component";
 
 @NgModule({
     declarations: [
-        SupplierListComponent
+        SupplierListComponent,
+        CreateSupplierComponent,
+        EditSupplierComponent
     ],
     imports: [
         CommonModule,

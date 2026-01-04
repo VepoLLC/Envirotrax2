97b45e548a9b348dd2a13326042f6e69acea1f09
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2026-01-04 10:03:31 -0800

    Changes for the auth project styles

--- src/Envirotrax.Auth/wwwroot/js/site.js
+++ src/Envirotrax.Auth/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showHidePassword(event, passwordId) {
+    var passwordElement = document.getElementById(passwordId);
+    var eyeIcon = event.querySelector('i');
+
+    if (passwordElement.type == 'password') {
+        passwordElement.type = 'text';
+        eyeIcon.className = 'fa fa-eye-slash'
+    } else {
+        passwordElement.type = 'password';
+        eyeIcon.className = "fa fa-eye"
+    }
+}

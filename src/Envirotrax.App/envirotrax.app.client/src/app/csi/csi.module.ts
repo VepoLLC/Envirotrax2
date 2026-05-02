7db720354f1dee3ee0c810adaa864068b7142f4e
Levon Adamyan <levon.adamyan@developerpartners.com>
2026-05-03 01:52:07 +0400

    [EN-58] made naming corrections, bind Active to hasCsiInspection

--- src/Envirotrax.App/envirotrax.app.client/src/app/csi/csi.module.ts
+++ src/Envirotrax.App/envirotrax.app.client/src/app/csi/csi.module.ts
@@ -7,13 +7,12 @@ import { CsiRoutingModule } from "./csi-routing.module";
 import { CsiInspectionListComponent } from "./inspections/list/csi-inspection-list.component";
 import { CsiInspectorListComponent } from "./inspectors/list/csi-inspector-list.component";
 import { CsiInspectorDetailsComponent } from "./inspectors/details/csi-inspector-details.component";
-import { CsiInspectorWaterSuppliersComponent } from "./inspectors/details/water-suppliers/csi-inspector-water-suppliers.component";
+import { CsiInspectorWaterSuppliersComponent } from "./inspectors/details/water-suppliers/list/csi-inspector-water-suppliers.component";
 import { CsiInspectorUsersComponent } from "./inspectors/details/users/csi-inspector-users.component";
-import { CsiInspectorLicenseInsuranceComponent } from "./inspectors/details/license-insurance/csi-inspector-license-insurance.component";
-import { CsiInspectorAddEditLicenseComponent } from "./inspectors/details/license-insurance/modals/add-edit-csi-inspector-license.component";
-import { CsiInspectorAddEditInsuranceComponent } from "./inspectors/details/license-insurance/modals/add-edit-csi-inspector-insurance.component";
-import { EditCsiInspectorWaterSupplierComponent } from "./inspectors/details/water-suppliers/modals/edit-csi-inspector-water-supplier.component";
-import { LicenseHelpTextComponent } from "../professionals/users/edit/licenses/help-text/license-help-text.component";
+import { CsiInspectorLicenseInsuranceComponent } from "./inspectors/details/license-insurance/list/csi-inspector-license-insurance.component";
+import { CsiInspectorAddEditLicenseComponent } from "./inspectors/details/license-insurance/edit/add-edit-csi-inspector-license.component";
+import { CsiInspectorAddEditInsuranceComponent } from "./inspectors/details/license-insurance/edit/add-edit-csi-inspector-insurance.component";
+import { EditCsiInspectorWaterSupplierComponent } from "./inspectors/details/water-suppliers/edit/edit-csi-inspector-water-supplier.component";
 import { ProfessionalModule } from "../professionals/professional.module";
 
 @NgModule({

e251dcde291cfc637f6c6d8042b7b2c82658e059
Gegham Gevorgyan <gegham.gevorgyan@developerpartners.com>
2026-06-11 16:52:00 +0400

    Add Images Section to CSI Inspections

---
+++ src/Envirotrax.App/Envirotrax.App.Server/Data/Migrations/20260611112801_UpdateCsiInspectionImageDescriptionLength.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Envirotrax.App.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCsiInspectionImageDescriptionLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "CsiInspectionImages",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "CsiInspectionImages",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+    }
+}

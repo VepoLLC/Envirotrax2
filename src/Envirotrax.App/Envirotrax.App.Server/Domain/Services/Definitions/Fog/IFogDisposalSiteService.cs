cd1f145f87d6ddd4cd90502e561f3646f6dc8950
Gegham Gevorgyan <gegham.gevorgyan@developerpartners.com>
2026-06-29 18:05:41 +0400

    Remove overrides

--- src/Envirotrax.App/Envirotrax.App.Server/Domain/Services/Definitions/Fog/IFogDisposalSiteService.cs
+++ src/Envirotrax.App/Envirotrax.App.Server/Domain/Services/Definitions/Fog/IFogDisposalSiteService.cs
@@ -1,4 +1,3 @@
-using DeveloperPartners.SortingFiltering;
 using Envirotrax.App.Server.Domain.DataTransferObjects.Fog;
 using Envirotrax.Common.Domain.Services.Defintions;
 
@@ -6,5 +5,4 @@ namespace Envirotrax.App.Server.Domain.Services.Definitions.Fog;
 
 public interface IFogDisposalSiteService : IService<FogDisposalSiteDto>
 {
-    Task<IPagedData<FogDisposalSiteDto>> GetActiveAsync(PageInfo pageInfo, Query query, CancellationToken cancellationToken);
 }

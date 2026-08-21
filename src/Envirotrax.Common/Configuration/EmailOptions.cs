bb22b964b0df85d925d00002133423fb9278687c
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2026-08-21 00:27:18 -0700

    EN-180 small change for noreply email address

--- src/Envirotrax.Common/Configuration/EmailOptions.cs
+++ src/Envirotrax.Common/Configuration/EmailOptions.cs
@@ -6,7 +6,7 @@ public class EmailOptions
 
     public string Endpoint { get; set; } = null!;
 
-    public string NoreplyAddress { get; set; } = "DoNotReply@mail.envirotrax.com";
+    public string NoreplyAddress { get; set; } = "noreply@mail.envirotrax.com";
     public string TeamAddress { get; set; } = "team@mail.envirotrax.com";
     public string InfoAddress { get; set; } = "info@mail.envirotrax.com";
 

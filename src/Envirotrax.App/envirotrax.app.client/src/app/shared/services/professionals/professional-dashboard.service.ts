996a5c586632b01ac15132825f93d4cc6ceb6fb8
Levon Adamyan <levon.adamyan@developerpartners.com>
2026-09-11 00:03:37 +0400

    [EN-210] change professional dashboard section to show licenses and insurances on one section without tabs

--- src/Envirotrax.App/envirotrax.app.client/src/app/shared/services/professionals/professional-dashboard.service.ts
+++ src/Envirotrax.App/envirotrax.app.client/src/app/shared/services/professionals/professional-dashboard.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { UrlResolverService } from '../helpers/url-resolver.service';
+import { QueryHelperService } from '../helpers/query-helper.service';
 import { ProfessionalDashboardStats } from '../../models/professionals/professional-dashboard-stats';
+import { ProfessionalDashboardLicenseInsurance } from '../../models/professionals/professional-dashboard-license-insurance';
+import { PagedData } from '../../models/paged-data';
+import { PageInfo } from '../../models/page-info';
+import { Query } from '../../models/query';
 
 @Injectable({
     providedIn: 'root'
@@ -10,11 +15,22 @@ import { ProfessionalDashboardStats } from '../../models/professionals/professio
 export class ProfessionalDashboardService {
     constructor(
         private readonly _http: HttpClient,
-        private readonly _urlResolver: UrlResolverService
+        private readonly _urlResolver: UrlResolverService,
+        private readonly _queryHelper: QueryHelperService
     ) {}
 
     public getStats(): Promise<ProfessionalDashboardStats> {
         const url = this._urlResolver.resolveUrl('/api/professionals/dashboard/stats');
         return lastValueFrom(this._http.get<ProfessionalDashboardStats>(url));
+    }
+
+    public getLicensesAndInsurances(pageInfo: PageInfo, query: Query): Promise<PagedData<ProfessionalDashboardLicenseInsurance>> {
+        const url = this._urlResolver.resolveUrl('/api/professionals/dashboard/licenses-and-insurances');
+
+        const observable = this._http.get<PagedData<ProfessionalDashboardLicenseInsurance>>(url, {
+            params: this._queryHelper.buildQuery(pageInfo, query)
+        });
+
+        return lastValueFrom(observable);
     }
 }
